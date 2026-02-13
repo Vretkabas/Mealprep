7066f51eaadf0bf552acf002a9c214cc8bf1f2fd
JAATAR <133091960+JAATAR@users.noreply.github.com>
2026-02-13 17:33:15 +0100

    Update profile page

--- frontend/mealprep_flutter/lib/settings/profile_page.dart
+++ frontend/mealprep_flutter/lib/settings/profile_page.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:image_picker/image_picker.dart';
-import 'edit_profile_page.dart'; // Zorg dat dit bestand bestaat
+import 'edit_profile_page.dart'; 
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
